a4fd83036c0e0b4f33afa95b2bc498101cb8310f
grbrmr <chrgro@gmx.de>
2022-05-16 16:35:08 +0200

    adapted for dataset_json

--- evaluation/create_evaluation_config.py
+++ evaluation/create_evaluation_config.py
@@ -1,5 +1,4 @@
 import json
-import pandas
 import os
 import argparse
 
@@ -8,8 +7,8 @@ import argparse
 def main(args):
     _version=0.1
     d_opt=vars(args)
-    csv_input=d_opt['csv_input']
-    output_dir=d_opt['output_dir']
+    pairs_config=args.pairs_config
+    output_dir=args.output_dir
     #evaluation_methods
     evals={}
     for eval in ['sdlogj','tre']:
@@ -23,31 +22,40 @@ def main(args):
                 evals[eval]=d_opt[eval]
 
     ##eval pairs
-    eval_pairs=[]
-    pairs = pandas.read_csv(csv_input)
-    for i,val in pairs.iterrows():
-        eval_pairs.append({
-            'fixed':val['fixed'],
-            'moving':val['moving']
-            })
+    with open(pairs_config, 'r') as f:
+        json_data=json.load(f)
+    if args.test:
+        eval_pairs = json_data['registration_test']
+        teststring='_TEST_'
+    else:
+        eval_pairs = json_data['registration_val']
+        teststring='_VAL_'
+
+    task_name=json_data['name']
+    if args.expected_shape is not None:
+        print('ff')
+        expected_shape = d_opt['expected_shape']
+    else:
+        expected_shape=json_data['tensorImageShape']["0"]
+        expected_shape.append(3)
 
     #write config
-    evaluation_config={ 'task_name':d_opt['task_name'],
+    evaluation_config={ 'task_name':task_name,
                         'evaluation_methods':evals,
-                        'expected_shape':d_opt['expected_shape'],
+                        'expected_shape':expected_shape,
                         'eval_pairs':eval_pairs,
                         'eval_config_version':_version
                         }
 
-    with open(os.path.join(output_dir,d_opt['task_name']+'_evaluation_config.json'), 'w') as f:
+    with open(os.path.join(output_dir,task_name+teststring+'evaluation_config.json'), 'w') as f:
         json.dump(evaluation_config,f, indent=4)
 
 if __name__ == "__main__":
-    parser=argparse.ArgumentParser(description="Create evaluation.json from pairs_{val/test}.csv ")
-    parser.add_argument("-c","--csv", dest="csv_input", help="path to pairs_{test/val}.csv", required=True)
-    parser.add_argument("-s","--shape", dest='expected_shape', help="expected shape of deformation field", required=True, nargs='+', type=int)
-    parser.add_argument("-n","--name", dest="task_name", help='Task/Evaluation name (e.g. OASIS)', required=True)
+    parser=argparse.ArgumentParser(description="Create evaluation.json from {TASKNAME}_dataset.json")
+    parser.add_argument("-c","--config", dest="pairs_config", help="path to {TASKNAME}_dataset.json", required=True)
     parser.add_argument("-o","--output", dest="output_dir", help="path to write evaluation.json", required=False, default='.')
+    parser.add_argument("-s","--shape", dest='expected_shape', help="expected shape of deformation field", required=False, nargs='+', type=int)
+    parser.add_argument("--test", dest="test", action=argparse.BooleanOptionalAction, default=False) #Otherwise Validation
 
     parser.add_argument("--SDlogJ", dest="sdlogj", help="Evaluate SDlogJ", action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument("--DSC", dest="dice", help="Specify the labels. If input is single int, is is assumed to be max label", nargs='+', type=int)
