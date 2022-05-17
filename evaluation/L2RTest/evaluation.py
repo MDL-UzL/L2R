a3cd372d6deca5d911eee0822c02089d76d66177
grbrmr <chrgro@gmx.de>
2022-05-17 12:45:12 +0200

    initial

--- evaluation/L2RTest/evaluation.py
+++ evaluation/L2RTest/evaluation.py
@@ -7,7 +7,6 @@ import pandas
 import argparse
 
 from utils import *
-from scipy.ndimage import map_coordinates, zoom
 from surface_distance import *
 
 import os
@@ -23,12 +22,14 @@ def main(args):
     DEFAULT_EVALUATION_OUTPUT_FILE_PATH=Path(d_opt['output_path'])
     
     #read evaluation_config.json
+    
     with open(DEFAULT_CONFIG_JSON_PATH, 'r') as f:
         data = json.load(f)
 
     name=data['task_name']
     expected_shape=np.array(data['expected_shape'])
-    evaluation_methods=list(data['evaluation_methods'].keys())
+    evaluation_methods_metrics=[tmp['metric'] for tmp in data['evaluation_methods']]
+
     eval_pairs=data['eval_pairs']
     len_eval_pairs=len(eval_pairs)
 
@@ -46,10 +47,8 @@ def main(args):
     cases_results=pandas.DataFrame()
 
     if verbose:
-        print(f"Evaluate for: {evaluation_methods}")
+        print(f"Evaluate {len_eval_pairs} cases for: {[tmp['name'] for tmp in data['evaluation_methods']]}")
     for idx, pair in enumerate(eval_pairs):
-        ## get paths
-        #print('case',idx)
         case_results={}
 
         fix_label_path=os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['fixed'].replace('images','labels'))
@@ -62,55 +61,48 @@ def main(args):
         if not (shape==expected_shape).all():
             raise_shape_error(disp_name, shape, expected_shape)
 
-        ## load files 
-        if any([True for eval_ in ['dice','dice_secret','hd95','hd95_secret', 'tre'] if eval_ in evaluation_methods]):
+        ## load neccessary files 
+        if any([True for eval_ in ['dice','hd95', 'tre'] if eval_ in evaluation_methods_metrics]):
             spacing_fix=nib.load(os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['fixed'])).header.get_zooms()[:3]
             spacing_mov=nib.load(os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['moving'])).header.get_zooms()[:3]
 
-        if any([True for eval_ in ['dice','dice_secret','hd95','hd95_secret'] if eval_ in evaluation_methods]):
+        if any([True for eval_ in ['dice','hd95'] if eval_ in evaluation_methods_metrics]):
             fixed_seg=nib.load(fix_label_path).get_fdata()
             moving_seg=nib.load(mov_label_path).get_fdata()
             warped_seg=warp(fixed_seg,moving_seg,disp_field,spacing_fix,spacing_mov)
                 
+        ## iterate over designated evaluation metrics
+        for _eval in data['evaluation_methods']:
+            _name=_eval['name']
 
-        ### SDlogJ
-        if 'sdlogj' in evaluation_methods:
-            jac_det = (jacobian_determinant(disp_field[np.newaxis, :, :, :, :].transpose((0,4,1,2,3))) + 3).clip(0.000000001, 1000000000)
-            log_jac_det = np.log(jac_det)
-            case_results['LogJacDetStd']=log_jac_det.std()
+            ### SDlogJ
+            if 'sdlogj' == _eval['metric']:
+                jac_det = (jacobian_determinant(disp_field[np.newaxis, :, :, :, :].transpose((0,4,1,2,3))) + 3).clip(0.000000001, 1000000000)
+                log_jac_det = np.log(jac_det)
+                case_results[_name]=log_jac_det.std()
 
-        ### DSC
-        if 'dice' in evaluation_methods:
-            labels = data['evaluation_methods']['dice']
-            dice = compute_dice(fixed_seg,moving_seg,warped_seg,labels)
-            case_results['DiceCoefficient']=dice
-        
-        ### DSC_Secret
-        if 'dice_secret' in evaluation_methods:
-            labels = data['evaluation_methods']['dice_secret']
-            dice_secret = compute_dice(fixed_seg,moving_seg,warped_seg,labels)
-            case_results['DiceCoefficient_Secret']=dice_secret
+            ### DSC
+            if 'dice' == _eval['metric']:
+                labels = _eval['labels']
+                dice = compute_dice(fixed_seg,moving_seg,warped_seg,labels)
+                case_results[_name]=dice
 
-        ### HD95
-        if 'hd95' in evaluation_methods:
-            labels = data['evaluation_methods']['hd95']
-            hd95 = compute_hd95(fixed_seg,moving_seg,warped_seg,labels)
-            case_results['HausdorffDistance95']=hd95
+            ### HD95
+            if 'hd95' == _eval['metric']:
+                labels = _eval['labels']
+                hd95 = compute_hd95(fixed_seg,moving_seg,warped_seg,labels)
+                case_results[_name]=hd95
         
-        ### HD95_Secret
-        if 'hd95_secret' in evaluation_methods:
-            labels = data['evaluation_methods']['hd95_secret']
-            hd95_secret = compute_hd95(fixed_seg,moving_seg,warped_seg,labels)
-            case_results['HausdorffDistance95_Secret']=hd95_secret
-        
-        ### TRE
-        if 'tre' in evaluation_methods:
-            lms_fix_path = os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['fixed'].replace('images', 'landmarks').replace('.nii.gz','.csv'))
-            lms_mov_path = os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['moving'].replace('images', 'landmarks').replace('.nii.gz','.csv'))
-            fix_lms = np.loadtxt(lms_fix_path, delimiter=',')
-            mov_lms = np.loadtxt(lms_mov_path, delimiter=',')
-            tre = compute_tre(fix_lms, mov_lms, disp_field ,spacing_fix, spacing_mov)
-            case_results['TRE']=tre.mean()
+            ### TRE
+            if 'tre' == _eval['metric']:
+                destination = _eval['dest']
+                lms_fix_path = os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['fixed'].replace('images', destination).replace('.nii.gz','.csv'))
+                lms_mov_path = os.path.join(DEFAULT_GROUND_TRUTH_PATH, pair['moving'].replace('images', destination).replace('.nii.gz','.csv'))
+                fix_lms = np.loadtxt(lms_fix_path, delimiter=',')
+                mov_lms = np.loadtxt(lms_mov_path, delimiter=',')
+                tre = compute_tre(fix_lms, mov_lms, disp_field ,spacing_fix, spacing_mov)
+                case_results[_name]=tre.mean()
+
         if verbose:
             print(f'case_results [{idx}]: {case_results}')
         cases_results=pandas.concat([cases_results, pandas.DataFrame(case_results, index=[0])], ignore_index=True)
--- evaluation/README.md
+++ evaluation/README.md
@@ -10,17 +10,20 @@ Structure:
 HowTo:
 1) Create evaluation_config.json with create_evaluation_config.py (or use from evaluation_configs)
 ```
-python create_evaluation_config.py -c AbdomenCTCT_dataset.json 
-  -o evaluation_configs/ 
-  --SDlogJ 
-  --DSC 13 
-  --HD95 13
+python create_evaluation_config.py -o evaluation_configs/ -c ../tmp/AbdomenCTCT_dataset.json --SDlogJ  --DSC DSC 13 --HD95 HD95 13  
+python create_evaluation_config.py -o evaluation_configs/ -c ../tmp/AbdomenMRCT_dataset.json --SDlogJ --DSC DSC 4 --HD95 HD95 4
+python create_evaluation_config.py -o evaluation_configs/ -c ../tmp/LungCT_dataset.json --SDlogJ --TRE TRE_lm landmarks --TRE TRE_kp keypoints
+python create_evaluation_config.py -o evaluation_configs/ -c ../tmp/CuRIOUS_dataset.json --SDlogJ --TRE TRE_lm landmarks
+python create_evaluation_config.py -o evaluation_configs/ -c ../tmp/HippocampusMR_dataset.json --SDlogJ --DSC DSC 2 --HD95 HD95 2
+python create_evaluation_config.py -o evaluation_configs/ -c ../tmp/OASIS_dataset.json --SDlogJ --DSC DSC 35 --HD95 HD95 35
 ```
+
 3) Run evaluation.py
 ```
  python evaluation.py -i zero_def_fields/HippocampusMR/ 
   -d .../HippocampusMR/ 
   -o output_test/Hippo.json 
-  -c evaluation_configs/HippocampusMR_Val_evaluation_config.json 
+  -c evaluation_configs/HippocampusMR_VAL_evaluation_config.json 
   -v
 ```
+
